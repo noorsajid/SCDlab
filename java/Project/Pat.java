661ab88197e6764569797d4dc55f59c752262144
noor sajid <noorsajidns324@gmail.com>
2023-01-04 02:45:24 +0500

    1.15 version

---
+++ java/Project/Pat.java
@@ -0,0 +1,16 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project;
+
+/**
+ *
+ * @author HP
+ */
+public class Pat implements Hospital {
+      public void makeObject(){
+        System.out.println(" ");
+    }
+}
